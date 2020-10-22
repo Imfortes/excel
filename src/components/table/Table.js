50358f44e5333c175937c1647b29557afd496fec
Imfortes <imfortes@list.ru>
2020-10-22 21:21:51 +0300

    "Finish table resize"

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,26 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {resizeHandler} from '@/components/table/table.resize';
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
 
+    constructor($root) {
+        super($root, {
+            name: 'Table',
+            listeners: ['mousedown']
+        })
+    }
 
     toHTML() {
-        return createTable(100)
+        return createTable(20)
     }
+
+    onMousedown(event) {
+        console.log('mousedown', event.target.getAttribute('data-resize'))
+        if (shouldResize(event)) {
+            resizeHandler(this.$root, event)
+        }
+    }
+
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -46,6 +46,36 @@ class Dom {
 
         return this
     }
+
+    get data() {
+        return this.$el.dataset
+    }
+
+    closest(selector) {
+        return $(this.$el.closest(selector))
+    }
+
+    getCoors() {
+        return this.$el.getBoundingClientRect()
+    }
+
+    findAll(selector) {
+        return this.$el.querySelectorAll(selector)
+    }
+
+    css(styles = {}) {
+        // for (const key in styles) {
+        //     if (styles.hasOwnProperty(key)) {
+        //         console.log(key)
+        //     }
+        // }
+        Object
+            .keys(styles)
+            .forEach(key => {
+            this.$el.style[key] = styles[key]
+        })
+        this.$el
+    }
 }
 
 // event.target
